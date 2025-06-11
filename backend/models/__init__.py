d8ade3f4b352152f372a8a3faf0192ab096165e4
Andrii Matsevytyi <macevitiya@i.ua>
2025-06-11 20:03:42 +0300

    added entities and db schemas of information categpry and discover source

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -58,3 +58,30 @@ class SearchHistory(db.Model):
             'report_id': self.report_id,
             'created_at': self.created_at.isoformat() + 'Z'
         }
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    weight = db.Column(db.Float, nullable=False)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'weight': self.weight
+        }
+        
+class Source(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
